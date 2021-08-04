a332333e0716cdb2149f7b4de443e55f98993f97
hzhan236 <hzhan236@uw.edu>
2021-08-04 14:30:11 -0700

    added memcached for friendship

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -11,6 +11,7 @@ from friendships.api.serializers import (
     FriendshipSerializerForCreate,
 )
 from django.contrib.auth.models import User
+from friendships.services import FriendshipService
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
@@ -57,6 +58,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
                 'errors': serializer.errors,
             }, status=status.HTTP_400_BAD_REQUEST)
         instance = serializer.save()
+        FriendshipService.invalidate_following_cache(request.user.id)
         return Response(
             FollowingSerializer(instance, context={'request': request}).data,
             status=status.HTTP_201_CREATED,
@@ -81,4 +83,5 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             from_user=request.user,
             to_user=pk,
         ).delete()
+        FriendshipService.invalidate_following_cache(request.user.id)
         return Response({'success': True, 'deleted': deleted})
